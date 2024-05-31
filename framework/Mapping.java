35621d65f491027e1dbdd290579985a51839c657
Dinantsoa <dinantsoa70@gmail.com>
2024-05-31 08:14:55 +0300

    sprint3

--- framework/Mapping.java
+++ framework/Mapping.java
@@ -1,20 +1,37 @@
-
 package mg.itu.prom16;
+
+import java.lang.reflect.*;
 
 public class Mapping {
     private String className;
     private String methodName;
+    private Class classe;
 
-    public Mapping(String className, String methodName) {
+    private Method methode;
+
+    public Mapping(String className, String methodName, Class classe, Method method) {
         this.className = className;
         this.methodName = methodName;
+        this.methode = method;
+        this.classe = classe;
     }
 
     public String getClassName() {
-        return className;
+        return this.className;
     }
 
     public String getMethodName() {
-        return methodName;
+        return this.methodName;
+    }
+
+    public String retour() {
+        String valiny = "";
+        try {
+            Object instance = this.classe.getDeclaredConstructor().newInstance();
+            valiny = this.methode.invoke(instance).toString();
+        } catch (Exception e) {
+            valiny = "Tsy mety";
+        }
+        return valiny;
     }
 }
